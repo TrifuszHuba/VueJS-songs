cb38120e6e9e431b13a006844ca2c2491b87dba3
Trifusz Huba <71632252+TrifuszHuba@users.noreply.github.com>
2024-12-16 21:00:53 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="container">
+    <ArtistSelect @artist-selected="artistSelected($event)"></ArtistSelect>
+    <SongList v-if="selectedArtist !== null" :artistId="selectedArtist"></SongList>
+  </div>
+</template>
+
+<script>
+import ArtistSelect from './components/ArtistSelect.vue';
+import SongList from './components/SongList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ArtistSelect,
+    SongList,
+  },
+  data() {
+    return {
+      selectedArtist: null,
+    };
+  },
+  methods: {
+    artistSelected(artistId) {
+      this.selectedArtist = artistId !== 'null' ? artistId : null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  #container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
